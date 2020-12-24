0950297c2891b382a71aacc07f0a9ab34acd33bf
unknown <stanislav.afanasyev99@mail.ru>
2020-12-24 17:12:04 +0300

    Added Navbar component name

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
   <div id="app">
-    <Navbar/>
+    <Navbar />
     <div class="container">
-      <router-view/>
+      <router-view />
     </div>
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
-
+import Navbar from '@/components/Navbar'
 export default {
-  components: {
-    Navbar
-  }
+  components: {Navbar}
 }
 </script>
 
 <style lang="scss">
-@import  "~materialize-css/dist/css/materialize.min.css";
+  @import "~materialize-css/dist/css/materialize.min.css";
 </style>
